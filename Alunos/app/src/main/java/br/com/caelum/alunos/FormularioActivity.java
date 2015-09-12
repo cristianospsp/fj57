9bb03be97d062893952388ec3d6a079b63dcd5b9
Cristiano de Oliveira <cristianospsp@gmail.com>
2015-09-12 12:24:31 -0300

    implementado, ActionBar e MenuInflate

--- Alunos/app/src/main/java/br/com/caelum/alunos/FormularioActivity.java
+++ Alunos/app/src/main/java/br/com/caelum/alunos/FormularioActivity.java
@@ -16,15 +16,15 @@ public class FormularioActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        View salvar = findViewById(R.id.formulario_botao);
-        final EditText nome = (EditText)findViewById(R.id.formulario_nome);
+        //View salvar = findViewById(R.id.formulario_botao);
+       // final EditText nome = (EditText)findViewById(R.id.formulario_nome);
 
-        salvar.setOnClickListener(new View.OnClickListener() {
+        /*salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this, "Salvar Estudante: " + nome.getText(), Toast.LENGTH_LONG ).show();
             }
-        });
+        });*/
     }
 
 
@@ -32,7 +32,7 @@ public class FormularioActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_formulario, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -40,13 +40,22 @@ public class FormularioActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(this, "OK - Clicado", Toast.LENGTH_LONG).show();
+                finish();
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
